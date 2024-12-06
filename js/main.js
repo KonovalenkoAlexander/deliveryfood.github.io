2fd1b90ac4480ac95b9ba51f26107404760d92a7
KonovalenkoAlexander <konovalenko06zp@gmail.com>
2024-12-06 13:07:29 +0200

    123

--- js/main.js
+++ js/main.js
@@ -171,3 +171,21 @@ const restaurants = [
 
 renderCards(restaurants);
 checkAuth();
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 3000,
+        },
+    });
+});
